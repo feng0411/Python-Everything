8588aefde76e947dff1d2f7b773ec256707751de
feng0411 <31056585+feng0411@users.noreply.github.com>
2017-08-27 14:51:18 +0800

    Update 抓斗鱼弹幕.py

--- douyu/抓斗鱼弹幕.py
+++ douyu/抓斗鱼弹幕.py
@@ -8,11 +8,11 @@ def danmu_fn(msg):
     client = msg['NickName']
     message = msg['Content']
     connection = pymysql.connect(
-            host='192.168.31.40',  # 连接的是本地数据库
-            user='feng',        # mysql用户名
-            passwd='19950411',  # 密码
-            db='feng',      # 数据库的名字
-            charset='utf8mb4',     # 默认的编码方式：
+            host='XXXX',  # 连接的是本地数据库
+            user='XXXX',        # mysql用户名
+            passwd='XXXX',  # 密码
+            db='XXXX',      # 数据库的名字
+            charset='utf8mb4',     # 默认的编码方式,最好选这个，因为弹幕会出现表情
             cursorclass=pymysql.cursors.DictCursor)
       
     with connection.cursor() as cursor:
